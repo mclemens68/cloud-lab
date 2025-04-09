cd947104406b22d33c72fcbde336034516c68299
brian1917 <brianpitta@gmail.com>
2025-04-09 11:41:17 -0400

    dns updates

--- terraform/aws_dns.tf
+++ terraform/aws_dns.tf
@@ -10,7 +10,7 @@ resource "aws_route53_record" "ec2_public_dns" {
   provider = aws.personal
   for_each = { for k, v in local.aws_config.ec2Instances : k => v if v.publicIP }
   zone_id  = data.aws_route53_zone.zone.zone_id
-  name     = "${each.key}${local.aws_config.dnsSuffix}"
+  name     = "${local.aws_config.dnsSubDomain}${each.key}"
   type     = "A"
   ttl      = "30"
   records  = [aws_instance.ec2s[each.key].public_ip]
@@ -20,7 +20,7 @@ resource "aws_route53_record" "ec2_private_dns" {
   provider     = aws.route53
   for_each = local.aws_config.ec2Instances
   zone_id  = data.aws_route53_zone.zone.zone_id
-  name     = each.value["publicIP"] ? "${each.key}-priv${local.aws_config.dnsSuffix}" : "${each.key}${local.aws_config.dnsSuffix}"
+  name     = each.value["publicIP"] ? "${local.aws_config.dnsSubDomain}${each.key}-priv" : "${local.aws_config.dnsSubDomain}${each.key}"
   type     = "A"
   ttl      = "30"
   records  = [aws_instance.ec2s[each.key].private_ip]
@@ -30,7 +30,7 @@ resource "aws_route53_record" "azure_vm_private_dns" {
   provider     = aws.route53
   for_each = merge(local.azure_config.linuxVMs, local.azure_config.windowsVMs)
   zone_id  = data.aws_route53_zone.zone.zone_id
-  name     = each.value["publicIP"] ? "${each.key}-priv${local.aws_config.dnsSuffix}" : "${each.key}${local.aws_config.dnsSuffix}"
+  name     = each.value["publicIP"] ? "${local.aws_config.dnsSubDomain}${each.key}-priv" : "${local.aws_config.dnsSubDomain}${each.key}"
   type     = "A"
   ttl      = "30"
   records  = [azurerm_network_interface.vminterfaces[each.key].private_ip_address]
@@ -40,7 +40,7 @@ resource "aws_route53_record" "azure_vm_public_dns" {
   provider     = aws.route53
   for_each = { for k, v in merge(local.azure_config.linuxVMs, local.azure_config.windowsVMs) : k => v if v.publicIP }
   zone_id  = data.aws_route53_zone.zone.zone_id
-  name     = "${each.key}${local.aws_config.dnsSuffix}"
+  name     = "${local.aws_config.dnsSubDomain}${each.key}"
   type     = "A"
   ttl      = "30"
   records  = [azurerm_public_ip.public_ip[each.key].ip_address]
@@ -50,7 +50,7 @@ resource "aws_route53_record" "rds_private_dns" {
   provider     = aws.route53
   for_each = local.aws_config.rdsInstances
   zone_id  = data.aws_route53_zone.zone.zone_id
-  name     = "${each.key}${local.aws_config.dnsSuffix}"
+  name     = "${local.aws_config.dnsSubDomain}${each.key}"
   type     = "CNAME"
   ttl      = "30"
   records  = [aws_db_instance.db_instances[each.key].address]
